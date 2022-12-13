d09cc70b5d2abfe654407f35477389b73e94273f
ilentiy <i.brykov9@bk.ru>
2022-12-13 11:01:25 +0300

    Debug

---
+++ myVK/myVK/Extension/DateFormatter+Extension.swift
@@ -0,0 +1,16 @@
+// DateFormatter+Extension.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+
+// MARK: Private Methods
+
+extension DateFormatter {
+    static func convert(_ date: Int) -> String {
+        let date = Date(timeIntervalSince1970: Double(date))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMMM yyyy, HH:mm "
+        dateFormatter.timeZone = .current
+        return dateFormatter.string(from: date)
+    }
+}
